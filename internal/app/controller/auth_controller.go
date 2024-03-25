5f1af2b749aa1900202e61786385776c947c5fe2
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-25 23:46:26 +0500

    implemented the auth controller

--- internal/app/controller/auth_controller.go
+++ internal/app/controller/auth_controller.go
@@ -1,14 +1,24 @@
 package controller
 
-import "github.com/gofiber/fiber/v3"
-
-type AuthController struct {
-}
+import (
+	"github.com/gofiber/fiber/v3"
+	"gorm.io/gorm"
+)
 
 type AuthControllerI interface {
 	Login(c fiber.Ctx) error
 	Register(c fiber.Ctx) error
 	Logout(c fiber.Ctx) error
+	RefreshToken(c fiber.Ctx) error
+	Restricted(c fiber.Ctx) error
+}
+
+type AuthController struct {
+	DB *gorm.DB
+}
+
+func NewAuthController(db *gorm.DB) *AuthController {
+	return &AuthController{DB: db}
 }
 
 func (ac *AuthController) Login(c fiber.Ctx) error {
