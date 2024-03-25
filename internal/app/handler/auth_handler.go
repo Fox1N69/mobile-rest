deb09d597d2e31111042b3c37ff5c39eee7b0bf9
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-26 01:26:04 +0500

    init controller in the handler

--- internal/app/handler/auth_handler.go
+++ internal/app/handler/auth_handler.go
@@ -3,13 +3,13 @@ package handler
 import "github.com/gofiber/fiber/v3"
 
 func (h *Handler) Login(c fiber.Ctx) error {
-	return nil
+	return h.AuthController.Login(c)
 }
 
 func (h *Handler) Register(c fiber.Ctx) error {
-	return nil
+	return h.AuthController.Register(c)
 }
 
 func (h *Handler) Logout(c fiber.Ctx) error {
-	return nil
+	return h.AuthController.Logout(c)
 }
