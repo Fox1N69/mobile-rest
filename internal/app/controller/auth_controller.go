1f9316cb787b1ebebdfceb359345dafd525c9fc9
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-25 23:40:46 +0500

    create auth controller func

--- internal/app/controller/auth_controller.go
+++ internal/app/controller/auth_controller.go
@@ -1,1 +1,24 @@
 package controller
+
+import "github.com/gofiber/fiber/v3"
+
+type AuthController struct {
+}
+
+type AuthControllerI interface {
+	Login(c fiber.Ctx) error
+	Register(c fiber.Ctx) error
+	Logout(c fiber.Ctx) error
+}
+
+func (ac *AuthController) Login(c fiber.Ctx) error {
+	return nil
+}
+
+func (ac *AuthController) Register(c fiber.Ctx) error {
+	return nil
+}
+
+func (ac *AuthController) Logout(c fiber.Ctx) error {
+	return nil
+}
