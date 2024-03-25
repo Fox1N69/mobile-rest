3c0135c87a5d0b19408550191e29fa17af95694a
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-25 23:38:25 +0500

    rename file handler and controller

--- internal/pkg/database/gormdb.go
+++ internal/pkg/database/gormdb.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	dsn := "user=postgres password=8008 dbname=mobilerest port=5432 sslmode=disable"
+	dsn := "user=postgres password=8008 dbname=kcpt port=5432 sslmode=disable"
 	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
