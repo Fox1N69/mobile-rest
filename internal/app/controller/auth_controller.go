efff7c61789f32d74bbe2e32c805bdb332460c00
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-26 09:31:01 +0500

    new code login and register

--- internal/app/controller/auth_controller.go
+++ internal/app/controller/auth_controller.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"encoding/json"
+	"mobile/internal/app/models"
 	"time"
 
 	"github.com/gofiber/fiber/v3"
@@ -54,11 +56,49 @@ func checkPassword(hashedPassword, password []byte) bool {
 
 func (ac *AuthController) Login(c fiber.Ctx) error {
 	//reading data from the body
-	return nil
+	loginData := new(models.LoginData)
+	if err := json.Unmarshal(c.Body(), &loginData); err != nil {
+		return err
+	}
+
+	//user search by username
+	var user models.User
+	result := ac.DB.Where("username = ?", &user).First(&user)
+	if result.Error != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "User not found"})
+	}
+
+	if !checkPassword(user.Password, loginData.Password) {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Uncorecct password"})
+	}
+
+	return c.JSON(fiber.Map{"message": "Autorization was successful", "user": user})
 }
 
 func (ac *AuthController) Register(c fiber.Ctx) error {
-	return nil
+	user := new(models.User)
+	if err := json.Unmarshal(c.Body(), &user); err != nil {
+		return err
+	}
+
+	// check for a username
+	var existingUser models.User
+	if result := ac.DB.Where("username = ?", user.Username).First(&existingUser); result.Error != nil {
+		return c.Status(fiber.StatusConflict).JSON(fiber.Map{"message": "A user with such an username alredy existing"})
+	}
+
+	//hashed password
+	hashedPassword, err := hashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+	user.Password = []byte(hashedPassword)
+
+	//create user
+	if result := ac.DB.Create(&user); result.Error != nil {
+		return c.Status(fiber.StatusConflict).JSON(fiber.Map{"message": "Error when creating user"})
+	}
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "User successfully create"})
 }
 
 func (ac *AuthController) Logout(c fiber.Ctx) error {
