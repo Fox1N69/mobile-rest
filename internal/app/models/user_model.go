79849eeed099d112084b357e204519c73f13cc38
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-26 09:14:29 +0500

    change user models

--- internal/app/models/user_model.go
+++ internal/app/models/user_model.go
@@ -2,6 +2,8 @@ package models
 
 type User struct {
 	ID       uint   `json:"id" gorm:"primaryKey"`
+	Fullname string `json:"fullname"`
+	Group    string `json:"group"`
 	Username string `json:"username"`
 	Password []byte `json:"passowrd"`
 }
