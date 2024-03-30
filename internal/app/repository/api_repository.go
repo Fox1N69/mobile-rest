6e5cd05fdcfdec90080cc1c2013520b0468dbfdb
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-30 08:36:22 +0500

    add api service and repository

---
+++ internal/app/repository/api_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import "gorm.io/gorm"
+
+type ApiRepository struct {
+	DB *gorm.DB
+}
+
+func NewApiRepository(db *gorm.DB) *ApiRepository {
+	return &ApiRepository{DB: db}
+}
---
+++ internal/app/service/api_service.go
@@ -0,0 +1,11 @@
+package service
+
+import "mobile/internal/app/repository"
+
+type ApiService struct {
+	repository repository.ApiRepository
+}
+
+func NewApiService(apiRepository repository.ApiRepository) *ApiService {
+	return &ApiService{repository: apiRepository}
+}
